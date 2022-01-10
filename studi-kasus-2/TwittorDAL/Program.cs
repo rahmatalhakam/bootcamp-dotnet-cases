e741eb3ee27bbd910582d2854ae4439803d806d4
rahmatalhakam <rahmatalhakam@gmail.com>
2022-01-10 12:27:13 +0700

    feat: init twittorDAL app 2

---
+++ studi-kasus-2/TwittorDAL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace TwittorDAL
+{
+  class Program
+  {
+    private static IConfigurationRoot _iconfiguration;
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+    private static void GetAppSettingsFile()
+    {
+      var builder = new ConfigurationBuilder()
+                           .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+      _iconfiguration = builder.Build();
+    }
+  }
+
+
+}
