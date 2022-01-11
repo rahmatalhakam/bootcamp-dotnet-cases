484c22f67b7452e6ad8eb48ff9c376213ea21bc0
rahmatalhakam <rahmatalhakam@gmail.com>
2022-01-11 14:28:16 +0700

    feat: initial TwittorLog project

---
+++ studi-kasus-2/TwittorLog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwittorLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
